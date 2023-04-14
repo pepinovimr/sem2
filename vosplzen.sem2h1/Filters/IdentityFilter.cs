2c84b7b8190fe081ce53764861eeaa9de6d2ffef
Josef Vimr <pepino.vimr@seznam.cz>
2023-04-14 12:54:00 +0200

    f

--- vosplzen.sem2h1/Filters/IdentityFilter.cs
+++ vosplzen.sem2h1/Filters/IdentityFilter.cs
@@ -5,6 +5,7 @@ using vosplzen.sem2h1.Services;
 
 namespace vosplzen.sem2h1.Filters
 {
+    [AttributeUsage(AttributeTargets.Method)]
     public class IdentityFilter : Attribute, IResourceFilter
     {
         public void OnResourceExecuted(ResourceExecutedContext context)
@@ -14,8 +15,9 @@ namespace vosplzen.sem2h1.Filters
 
         public void OnResourceExecuting(ResourceExecutingContext context)
         {
+            ///NEFUNGUJE!!!
             var _authorizationService = (AuthorizationService?)context.HttpContext.RequestServices.GetService(typeof(AuthorizationService));
-
+            
             if (_authorizationService is null)
             {
                 context.Result = new ContentResult
@@ -25,7 +27,7 @@ namespace vosplzen.sem2h1.Filters
                 return;
             }
 
-            context = _authorizationService.AuthorizeToken(context);
+            _authorizationService.AuthorizeToken(context);
         }
     }
 }
